b41792259887536ad4cddc5d5ba17c7d754b8429
A.A. Suvorov <smartlegiondev@gmail.com>
2025-08-24 06:39:38 +0700

    Smart Babylon Library v 0.6.4

--- smart_babylon_library/__init__.py
+++ smart_babylon_library/__init__.py
@@ -14,7 +14,7 @@ from .babylon_library import BabylonLibrary, BabylonLibraryIterator
 from .smart_babylon_library import SmartBabylonLibrary, SmartBabylonLibraryIterator
 from .config import *
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 __author__ = 'A.A. Suvorov'
 __all__ = [
     "LibraryStructure",
